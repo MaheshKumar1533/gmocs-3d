8cddebc84a7bb77d61548fbb181d8a5cf2b30967
V Mahesh Kumar <maheshkumarvmk0@gmail.com>
2025-04-09 14:01:55 +0530

    Updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
 import Spline from "@splinetool/react-spline";
 
 export default function Home() {
+	if (window.innerWidth < 480) {
+		window.location.href = "/mobile";
+	}
 	return (
 		<Spline
 			scene="https://prod.spline.design/bwaZOv-eoBjL9PSP/scene.splinecode"
-			style={{ width: "100vw", height: "100vh" }}
+			style={{ width: "100vw", height: "100vh", background: "#1B1A1C" }}
 		/>
 	);
 }
