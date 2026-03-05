9e84d47289096a0a0555740df6f2267a055ad592
Mahesh <maheshkumarvmk0@gmail.com>
2026-03-05 15:06:30 +0530

    GMOCS

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
 			<div className="home">
 				<div className="hero-section">
 					<Spline
-						scene="https://prod.spline.design/bwaZOv-eoBjL9PSP/scene.splinecode"
+						scene="https://prod.spline.design/lcX0Rs3X3ahwS4Sm/scene.splinecode"
 						style={{
 							width: "100vw",
 							height: "100vh",
